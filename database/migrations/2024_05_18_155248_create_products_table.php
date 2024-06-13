7ec388c4763e90493df4666dc09aac477a1b190e
ChrisMaia <anny.lmaia@gmail.com>
2024-06-13 16:18:19 -0300

    continuação

--- database/migrations/2024_05_18_155248_create_products_table.php
+++ database/migrations/2024_05_18_155248_create_products_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('category');
+            //$table->string('category');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->decimal('price', places: 2);
             $table->timestamps();
         });
